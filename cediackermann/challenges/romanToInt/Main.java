3caa17b54b261326c8255ac66f605e5f8fd1c230
ackermanncedi <ackermanncedi@gmail.com>
2024-12-02 13:59:21 +0000

    Add slow solution to romanToInt

---
+++ cediackermann/challenges/romanToInt/Main.java
@@ -0,0 +1,11 @@
+package cediackermann.challenges.romanToInt;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(        
+            sol.romanToInt("MCMXCIV")
+        );        
+    }
+    
+}
